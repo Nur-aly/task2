597f45e41127c2d047d490d8377911d11152c16a
Nur-aly <chiketaevn@gmail.com>
2020-09-22 15:48:32 +0600

    first commit

---
+++ task2.5.py
@@ -0,0 +1,9 @@
+
+
+x=int(input('Enter the number: '))
+if x>0:
+    print(1)
+elif x<0:
+    print(-1)
+else:
+    print(0)
---
+++ task2.6.py
@@ -0,0 +1,5 @@
+n=int(input("Enter the number: "))
+facrotial=1
+for i in range (1,n+1):
+    facrotial*=i
+print(facrotial)
